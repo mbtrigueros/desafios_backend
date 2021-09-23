5415a58b184d167348d584e23557ddcb5754919c
María Belén Trigueros <bel.trigueros@gmail.com>
2021-09-23 13:51:02 -0300

    Desafio11-Ejs

---
+++ desafio-11/01-ejs.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import ejs from 'ejs';
+
+
+// SERVER
+
+const app = express();
+const PORT = 8080;
+
+const server = app.listen(PORT, ()=>{
+    console.log('Servidor HTTP escuchando en el puerto', server.address().port);
+});
+server.on('error', error=>console.log('Error en servidor', error));
+
+// EJS
+
+app.set("view engine", "ejs");
+
+app.set("views", "./views"); // registra el motor de plantillas
+
+
+// ROUTER 
+
+const routerProductos = express.Router();
+
+// MIDDLEWARES
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', routerProductos);
+app.use(express.static('public'));
+
+// IMPORTO LAS RUTAS DESPUES DEL ROUTER 
+
+import rutas from './rutas.js';
+
+// PROGRAMA
+
+routerProductos.get(rutas.listar, rutas.funcionListar);
+routerProductos.get(rutas.vista, rutas.funcionVista);
+routerProductos.get(rutas.productosID, rutas.funcionID);
+routerProductos.post(rutas.guardar, rutas.funcionGuardar);
+routerProductos.put(rutas.actualizar, rutas.funcionActualizar);
+routerProductos.delete(rutas.borrar, rutas.funcionBorrar);
